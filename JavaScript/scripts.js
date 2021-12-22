950719e677d5b0cc48fc8d6c46633458828ce99d
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2021-12-22 14:47:50 -0300

    javascripts desafios

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,8 +1,58 @@
-var n1 = 3;
-    n2 = 5;
+//--------------desafio vogal ou consoante
 
+var letter = 'f';
+
+switch (letter) {
+  case 'a':
+    console.log("A - É UMA VOGAL");break;
+  case 'e':
+    console.log("E - É UMA VOGAL");break;
+  case 'i':
+    console.log("I - É UMA VOGAL");break;
+  case 'o':
+    console.log("O - É UMA VOGAL");break;
+  case 'u':
+    console.log("U - É UMA VOGAL");break;
+  default:
+    console.log("A LETRA DIGITADA É UMA CONSOANTE");
+}
+
+//------------------------
+
+// var n1 = 3;
+//     n2 = 5;
+
+/*
 if (n1 > n2){
     console.log('n1 é maior');
 } else{
     console.log("n1 é menor");
 }
+
+n1 > n2 ? console.log('n1 é maior') : console.log('n1 é menor') */
+
+// var maior = n1 > n2 ? n1 : n2;
+
+//------------------
+
+//  var n1 = 500;
+
+//  switch(n1){
+//     case 1000:
+//          console.log('GANHA UMA SMART TV'); break;
+//     case 500:
+//          console.log('GANHA UM NOTEBOOK'); break;
+//     case 300:
+//          console.log('GANHA UM CELULAR'); break;
+//     default: console.log('Outro valor');
+//  }
+
+//--------------desafio par ou impar
+
+// var num = 5;
+
+// if (num % 2 === 0) {
+//     console.log("O numero é par");
+// } else {
+//     console.log("O numero é impar");
+// }
