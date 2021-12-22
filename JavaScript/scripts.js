558513db60335814aa931403932cc93cab85911f
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2021-12-22 15:14:51 -0300

    new commit

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,21 +1,67 @@
+/* desafio
+
+Dado um valor inteiro n, complete o código abaixo de acordo com as condições:
+Se n for ímpar, exiba no console: "Estranho";
+Se n for par e for menor que 10, exiba no console: "Não é estranho";
+Se n for par e estiver entre 10 e 20, exiba no console: "Estranho";
+Se n for par e for maior que 20, exiba no console: "Não é estranho". */
+
+
+// if (n1 > n2) {
+//   console.log("n1 é maior");
+// } else {
+//   console.log("n1 é menor");
+// }
+
+// var num = n;
+
+// if (num % 2 !== 0){
+//     console.log("Estranho");break;
+//   } else if (num % 2 == 0 && num < 10){
+//     console.log("Não é estranho"); break;
+//   } else if (num % 2 == 0 && num >= 10 && num <= 20){
+//     console.log("Estranho");break;
+//   } else if (num % 2 == 0 && num > 20){
+//     console.log("Não é estranho");break;
+// }
+
 //--------------desafio vogal ou consoante
 
-var letter = 'f';
+// var letter = 'f';
 
-switch (letter) {
-  case 'a':
-    console.log("A - É UMA VOGAL");break;
-  case 'e':
-    console.log("E - É UMA VOGAL");break;
-  case 'i':
-    console.log("I - É UMA VOGAL");break;
-  case 'o':
-    console.log("O - É UMA VOGAL");break;
-  case 'u':
-    console.log("U - É UMA VOGAL");break;
-  default:
-    console.log("A LETRA DIGITADA É UMA CONSOANTE");
-}
+// switch (letter) {
+//   case 'a':
+//     console.log("A - É UMA VOGAL");break;
+//   case 'e':
+//     console.log("E - É UMA VOGAL");break;
+//   case 'i':
+//     console.log("I - É UMA VOGAL");break;
+//   case 'o':
+//     console.log("O - É UMA VOGAL");break;
+//   case 'u':
+//     console.log("U - É UMA VOGAL");break;
+//   default:
+//     console.log("A LETRA DIGITADA É UMA CONSOANTE");
+// }
+
+//------------------------outro jeito de fazer o desafio
+
+// var letter = 's',
+//     isVowel = false;
+
+// switch(letter){
+//     case 'a': isVowel = true; break;
+//     case 'e': isVowel = true; break;
+//     case 'i': isVowel = true; break;
+//     case 'o': isVowel = true; break;
+//     case 'u': isVowel = true; break;
+// }
+
+// if(isVowel){
+//     console.log('É UMA VOGAL');
+// }else{
+//     console.log('É UMA CONSOANTE');
+// }
 
 //------------------------
 
